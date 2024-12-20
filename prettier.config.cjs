53f0128174ec08b0704163efa5842e23a22379df
milkbottle0305 <bigtog0602@gmail.com>
2024-12-20 22:58:31 +0900

    chore: prettier tailwind 설치

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
