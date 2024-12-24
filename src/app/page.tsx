ca13b230eab2ffecdbb71ad736c559f81802c3f2
milkbottle0305 <bigtog0602@gmail.com>
2024-12-24 10:43:45 +0900

    feat: 디자인 수정정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { LoffiNotice } from './shared/components/LoffiNotice';
 import { LostarkNotice } from './shared/components/LostarkNotice';
 import QueryProviders from './shared/providers/QueryProviders';
 
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="flex h-[500px] w-full flex-col items-center justify-center">
       <QueryProviders>
         <LostarkNotice />
+        <LoffiNotice />
       </QueryProviders>
     </div>
   );
--- src/app/shared/components/LoffiNotice.tsx
+++ src/app/shared/components/LoffiNotice.tsx
@@ -3,16 +3,19 @@ import { useGetNoticesQuery } from '../queries/useGetNoticesQuery';
 import { SkeletonView } from '../ui/Skeleton';
 import { TitleInfoList } from '../ui/TitleInfoList';
 
-const NOTICES_COUNT = 10;
+const NOTICES_COUNT = 5;
 
-export const LostarkNotice = () => {
-  const { data: _notices, isLoading } = useGetNoticesQuery();
+export const LoffiNotice = () => {
+  const { data: _notices, isLoading } = useGetNoticesQuery({
+    searchText: '업데이트',
+    type: '공지',
+  });
 
   const notices = _notices?.slice(0, NOTICES_COUNT);
 
   return (
     <TitleInfoList
-      title={'로스트아크 공지사항'}
+      title={'로피 공지사항'}
       renderInfoList={() => {
         if (isLoading) {
           return <SkeletonView width="100%" height="20px" gap="4px" length={NOTICES_COUNT} />;
@@ -20,11 +23,8 @@ export const LostarkNotice = () => {
         return (
           <ul className="flex w-full flex-col items-start gap-1">
             {notices?.map((notice) => (
-              <li key={notice.Title} className="flex w-full items-center truncate">
-                <span className="mr-2 rounded-full bg-gray-100 px-1 text-xs text-gray-500">
-                  {notice.Type}
-                </span>
-                <p className="text-sm">{notice.Title}</p>
+              <li key={notice.Title} className="w-full">
+                <p className="w-full truncate text-sm">{notice.Title}</p>
               </li>
             ))}
           </ul>
